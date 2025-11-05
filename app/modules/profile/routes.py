4ce9b5d3090ede0264b1317c7daaf1fe5c0c7f06
loubna3 <loufouela@alum.us.es>
2025-11-05 21:29:56 +0100

    fix: fixing code format lint errors

--- app/modules/profile/routes.py
+++ app/modules/profile/routes.py
@@ -59,18 +59,19 @@ def my_profile():
 @profile_bp.route("/profile/<int:user_id>", methods=["GET"], endpoint="public_profile")
 def public_profile(user_id: int):
     user = User.query.get_or_404(user_id)
+
     page = request.args.get("page", 1, type=int)
     per_page = 10
+
     q = DataSet.query.filter_by(user_id=user.id).order_by(DataSet.created_at.desc())
     pagination = q.paginate(page=page, per_page=per_page)
     total_datasets = q.count()
 
     return render_template(
         "profile/summary.html",
-        user_profile=user.profile,   
-        user=user,                   
-        datasets=pagination.items,  
+        user_profile=user.profile,
+        user=user,
+        datasets=pagination.items,
         pagination=pagination,
         total_datasets=total_datasets,
     )
-
