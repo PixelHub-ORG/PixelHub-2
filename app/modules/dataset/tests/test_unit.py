0c5cb512a0a3dd1ce7d392a804b9d0cb92eef58e
ClaudiaOviedoGovantes <claudiaogvizcaino@gmail.com>
2025-11-08 18:17:33 +0100

    Test: dded more unit test for leaderboard

--- app/modules/dataset/tests/test_unit.py
+++ app/modules/dataset/tests/test_unit.py
@@ -65,3 +65,34 @@ def test_get_dataset_leaderboard_with_same_downloads(
     assert leaderboard_data[0].id <= leaderboard_data[1].id <= \
            leaderboard_data[2].id
 
+def test_get_dataset_leaderboard_already_sorted(dataset_service,
+                                                mock_dsdownloadrecord_repository):
+    mock_dataset_1 = MagicMock(spec=DataSet, id=1, downloads=30)
+    mock_dataset_2 = MagicMock(spec=DataSet, id=2, downloads=20)
+    mock_dataset_3 = MagicMock(spec=DataSet, id=3, downloads=10)
+    mock_dsdownloadrecord_repository.top_3_dowloaded_datasets_per_week.\
+        return_value = [mock_dataset_1, mock_dataset_2, mock_dataset_3]
+    period = "week"
+    leaderboard_data = dataset_service.get_dataset_leaderboard(period=period)
+    assert leaderboard_data[0].downloads > \
+           leaderboard_data[1].downloads > leaderboard_data[2].downloads
+
+
+def test_get_dataset_leaderboard_large_number_of_datasets(
+        dataset_service, mock_dsdownloadrecord_repository):
+    mock_datasets = [MagicMock(spec=DataSet, id=i, downloads=100)
+                     for i in range(1000)]
+    mock_dsdownloadrecord_repository.top_3_dowloaded_datasets_per_week.\
+        return_value = mock_datasets[:3]
+    period = "week"
+    leaderboard_data = dataset_service.get_dataset_leaderboard(period=period)
+    assert len(leaderboard_data) == 3
+
+
+def test_get_dataset_leaderboard_with_null_data(dataset_service,
+                                                mock_dsdownloadrecord_repository):
+    mock_dsdownloadrecord_repository.top_3_dowloaded_datasets_per_week.\
+        return_value = None
+    period = "week"
+    leaderboard_data = dataset_service.get_dataset_leaderboard(period=period)
+    assert leaderboard_data is None
